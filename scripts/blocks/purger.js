14b8f6f2fcf6cc12d0203ed672687046f37e9d1a
Arkanic <50847107+Arkanic@users.noreply.github.com>
2020-02-01 16:32:05 +1300

    hggg

--- scripts/blocks/purger.js
+++ scripts/blocks/purger.js
@@ -1,23 +1,24 @@
-const purgerShootEffect = newEffect(20, e => {
-    Draw.color(Color.red, Color.blue, e.fin());
+
+const siloLaunchEffect = newEffect(20, e => {
+    Draw.color(Color.white, Color.lightGray, e.fin());
     Lines.stroke(e.fout() * 3);
     Lines.circle(e.x, e.y, e.fin() * 100);
 });
 
-const purger = extendContent(Block, "purger", {
-    buildConfiguration(tile, table) {
-        table.addImageButton(Icon.upOpen, Styles.clearTransi, run(() => {
-            tile.configure(0);
-        })).size(50).disabled(boolf(b => tile.entity != null && !tile.entity.cons.valid()));
-    },
 
-    configured(tile, value) {
-        if(tile.entity.cons.valid()) {
-            Effects.effect(purgerShootEffect, tile);
-            for(let i = 0; i < 15; i++) {
-                Calls.createBullet(Bullets.flakExplosive, tile.getTeam(), tile.drawx(), tile.drawy(), Mathf.random(360), Mathf.random(0.5, 1.0), Mathf.random(0.2, 1.0));
+const purger = extendContent(Block, "purger", {
+    buildConfiguration(tile, table){
+        table.addImageButton(Icon.arrowUpSmall, Styles.clearTransi, run(() => {
+            tile.configure(0)
+        })).size(50).disabled(boolf(b => !tile.entity.cons.valid()))
+    },
+    configured(tile, value){
+        if(tile.entity.cons.valid()){
+            Effects.effect(siloLaunchEffect, tile)
+            for(var i = 0; i < 15; i++){
+                Calls.createBullet(Bullets.flakExplosive, tile.getTeam(), tile.drawx(), tile.drawy(), Mathf.random(360), Mathf.random(0.5, 1.0), Mathf.random(0.2, 1.0))
             }
-            tile.entity.cons.trigger();
+            tile.entity.cons.trigger()
         }
     }
-});
+})
